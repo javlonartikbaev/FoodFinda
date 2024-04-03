662374bf6169c2fc667085d8c66aa7e13e89d418
javlonartikbaev <artjavlon@gmail.com>
2024-04-03 09:41:37 +0500

    changed structure models (Orders, Category, Cart. ImgItems)

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,33 +1,58 @@
 from django.contrib import admin
 from .models import *
+
 # Register your models here.
 
 
 class AdminRestaurant(admin.ModelAdmin):
-    list_display = ('name_restaurant', 'address','start_date','end_date', 'img_restaurant','status')
+    list_display = (
+        "name_restaurant",
+        "address",
+        "start_time",
+        "end_time",
+        "img_restaurant",
+        "status",
+    )
 
 
 class AdminCategories(admin.ModelAdmin):
-    list_display = ('name_category',)
+    list_display = ("name_category", "img_category")
 
 
 class AdminMenuItem(admin.ModelAdmin):
-    list_display = ('name_item', 'price', 'quantity', 'info', 'restaurant','category')
+    list_display = (
+        "name_item",
+        "price",
+        "quantity",
+        "info",
+        "restaurant",
+        "category",
+    )
+
 
 class AdminImgItems(admin.ModelAdmin):
-    list_display = ('img_item', )
+    list_display = ("img_item",)
+
 
 class AdminBaskets(admin.ModelAdmin):
-    list_display = ('menu_items',)
+    list_display = ("menu_items", "price", "quantity")
+
 
 class AdminOrders(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'phone', 'price', 'quantity', 'status')
+    list_display = (
+        "first_name",
+        "last_name",
+        "phone",
+        "total_price",
+        "status",
+    )
+
 
 class AdminUsersFeedback(admin.ModelAdmin):
-    list_display = ('name', 'phoneNumber', 'email', 'message')
+    list_display = ("name", "phoneNumber", "email", "message")
 
 
-admin.site.register(Restaurant,AdminRestaurant)
+admin.site.register(Restaurant, AdminRestaurant)
 admin.site.register(Categories, AdminCategories)
 admin.site.register(MenuItems, AdminMenuItem)
 admin.site.register(ImgItems, AdminImgItems)
--- delivery/models.py
+++ delivery/models.py
@@ -1,104 +1,103 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 
 class Restaurant(models.Model):
-    STATUS_CHOICES = [
-        ('открыто', 'Открыто'),
-        ('закрыто', 'Закрыто')
-    ]
+    STATUS_CHOICES = [("открыто", "Открыто"), ("закрыто", "Закрыто")]
 
     name_restaurant = models.CharField(max_length=240)
     address = models.CharField(max_length=240)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    img_restaurant = models.CharField(max_length=254)
-    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='закрыто')
+    start_time = models.TimeField(default=datetime.now)
+    end_time = models.TimeField(default=datetime.now)
+    img_restaurant = models.ImageField(upload_to="delivery/img/", default="")
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default="закрыто")
 
     def __str__(self):
         return self.name_restaurant
 
     class Meta:
-        db_table = 'restaurant'
-        verbose_name = 'restaurant'
-        verbose_name_plural = 'restaurants'
-        ordering = ['status']
+        db_table = "restaurant"
+        verbose_name = "restaurant"
+        verbose_name_plural = "restaurants"
+        ordering = ["status"]
 
 
 class Categories(models.Model):
     name_category = models.CharField(max_length=160)
+    img_category = models.ImageField(upload_to="delivery/img_category/", default="")
 
     class Meta:
-        db_table = 'category'
-        verbose_name = 'category'
-        verbose_name_plural = 'categories'
+        db_table = "category"
+        verbose_name = "category"
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.name_category
 
 
+class ImgItems(models.Model):
+    img_item = models.ImageField(upload_to="delivery/img_items/img/", default="")
+
+    class Meta:
+        db_table = "img_items"
+        verbose_name = "img_item"
+        verbose_name_plural = "img_items"
+
+
 class MenuItems(models.Model):
     name_item = models.CharField(max_length=240)
     price = models.CharField(max_length=240)
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(default=0, blank=True)
     info = models.TextField()
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, default='')
-    category = models.ForeignKey(Categories, on_delete=models.CASCADE, default='')
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, default="")
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, default="")
+    img_items = models.ManyToManyField(ImgItems)
 
     class Meta:
-        db_table = 'menu_items'
-        verbose_name = 'menu_item'
-        verbose_name_plural = 'menu_items'
+        db_table = "menu_items"
+        verbose_name = "menu_item"
+        verbose_name_plural = "menu_items"
 
     def __str__(self):
         return self.name_item
 
 
-class ImgItems(models.Model):
-    img_item = models.CharField(max_length=255)
-    menu_item = models.ManyToManyField(MenuItems)
-
-    class Meta:
-        db_table = 'img_items'
-        verbose_name = 'img_item'
-        verbose_name_plural = 'img_items'
-
-    def __str__(self):
-        return self.img_item
-
-
 class Basket(models.Model):
     menu_items = models.ForeignKey(MenuItems, on_delete=models.CASCADE)
+    price = models.FloatField(default=0)
+    quantity = models.IntegerField(default=0, blank=True)
 
     class Meta:
-        db_table = 'basket'
-        verbose_name = 'basket'
-        verbose_name_plural = 'baskets'
+        db_table = "basket"
+        verbose_name = "basket"
+        verbose_name_plural = "baskets"
 
 
 class Orders(models.Model):
     STATUS_CHOICES = [
-        ('в обработке', 'В обработке'),
-        ('В пути', 'В пути'),
-        ('доставлен', 'Доставлен '),
-        ('отменен ', 'Отменен ')
+        ("в обработке", "В обработке"),
+        ("В пути", "В пути"),
+        ("доставлен", "Доставлен "),
+        ("отменен ", "Отменен "),
     ]
 
     first_name = models.CharField(max_length=99)
     last_name = models.CharField(max_length=99)
     phone = models.CharField(max_length=13)
     address = models.CharField(max_length=140)
-    price = models.CharField(max_length=255)
-    quantity = models.IntegerField()
+    total_price = models.FloatField(default=0)
     order_date = models.DateField()
-    status = models.CharField(choices=STATUS_CHOICES, max_length=100, default='в обработке')
+    status = models.CharField(
+        choices=STATUS_CHOICES, max_length=100, default="в обработке"
+    )
     basket = models.ManyToManyField(Basket)
 
     class Meta:
-        db_table = 'orders'
-        verbose_name = 'order'
-        verbose_name_plural = 'orders'
+        db_table = "orders"
+        verbose_name = "order"
+        verbose_name_plural = "orders"
 
 
 class Users(models.Model):
@@ -108,6 +107,6 @@ class Users(models.Model):
     message = models.TextField(max_length=450)
 
     class Meta:
-        db_table = 'users'
-        verbose_name = 'user'
-        verbose_name_plural = 'users'
+        db_table = "users"
+        verbose_name = "user"
+        verbose_name_plural = "users"
