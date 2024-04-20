c113f6a204cc0378cb5ec06e7bdab9e20c5886b5
javlonartikbaev <artjavlon@gmail.com>
2024-04-20 18:36:35 +0500

    admin panel

--- foodFinda/urls.py
+++ foodFinda/urls.py
@@ -7,4 +7,5 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("delivery.urls")),
+    path("", include('adminPanel.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
