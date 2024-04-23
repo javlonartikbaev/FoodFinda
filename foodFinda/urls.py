fb91ab52d887be0bc6f53045bb12a4b091948277
Ulmeken Kozybaeva <136685941+ulmekenkozybaeva@users.noreply.github.com>
2024-04-23 15:19:21 +0500

    Update urls.py

--- foodFinda/urls.py
+++ foodFinda/urls.py
@@ -16,7 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from delivery.views import *
+from delivery import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", views.main_page, name="main_page"),
+    path("basket/", views.basket, name="basket"),
+    path("categories/", views.categories, name="categories"),
+    path("contact/", views.contact, name="contact"),
+    path("restaurants/", views.restaurants, name="restaurants"),
+    path(
+        "restaurants/<slug:restaurant_slug>",
+        views.selected_restaurant,
+        name="selected_restaurant",
+    ),
+    path("menu_items/<slug:item_slug>/", views.menu_items, name="menu_items"),
 ]
